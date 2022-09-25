dba8c4201092fcd219591ad8824db30bb44d88f3
Lucas Valente <lucas.valente@asksuite.com>
2022-09-25 19:59:30 -0300

    :sparkles: feat: added the new routes on route index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import paymentMethodsRoutes from './payment_methods.routes';
 import peopleRoutes from './people.routes';
 import professionalsRoutes from './professionals.routes';
+import professionalsSpecialtiesRoutes from './professionals_specialties.routes';
+import serviceLocationsRoutes from './service_locations.routes';
 import specialtiesRoutes from './specialties.routes';
 import usersRoutes from './users.routes';
 
@@ -10,5 +13,8 @@ router.use('/users', usersRoutes);
 router.use('/people', peopleRoutes);
 router.use('/specialties', specialtiesRoutes);
 router.use('/professionals', professionalsRoutes);
+router.use('/paymentMethods', paymentMethodsRoutes);
+router.use('/serviceLocations', serviceLocationsRoutes);
+router.use('/professionals/specialties', professionalsSpecialtiesRoutes);
 
 export default router;
