c30649a07f53e0edf260c776cbc97e1ef104f3bb
Lucas Valente <lucas.valente@asksuite.com>
2022-09-24 21:24:34 -0300

    feat: created app error component

---
+++ src/middlewares/catchError.ts
@@ -0,0 +1,18 @@
+import { AppError } from '@errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+
+export async function catchError(
+  err: Error | AppError,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+}
