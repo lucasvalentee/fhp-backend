61953e3a24551e7133d90f336582703c06e9ec41
Lucas Valente <lucas.valente@asksuite.com>
2022-09-24 21:26:32 -0300

    feat: created the users and people migrations

---
+++ src/infra/typeorm/migrations/1664063022491-CreateUsers.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1664063022491 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'is_admin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
---
+++ src/infra/typeorm/migrations/1664064668918-CreatePeople.ts
@@ -0,0 +1,86 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePeople1664064668918 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'people',
+        columns: [
+          {
+            name: 'cpf',
+            type: 'varchar',
+            length: '11',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '250',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '250',
+          },
+          {
+            name: 'phone_number',
+            type: 'varchar',
+          },
+          {
+            name: 'country_state_id',
+            type: 'integer',
+          },
+          {
+            name: 'zip_code',
+            type: 'varchar',
+          },
+          {
+            name: 'city_id',
+            type: 'integer',
+          },
+          {
+            name: 'district',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '250',
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            length: '250',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_User_Person',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('people');
+  }
+}
