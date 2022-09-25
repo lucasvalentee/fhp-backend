bf8e00372042595e23b5330eef1ad8f06714c782
Lucas Valente <lucas.valente@asksuite.com>
2022-09-25 19:59:13 -0300

    :sparkles: feat: added the new repositories on container register singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,15 @@
+import PaymentMethodsRepository from 'infra/typeorm/repositories/PaymentMethodsRepository';
 import PeopleRepository from 'infra/typeorm/repositories/PeopleRepository';
 import ProfessionalsRepository from 'infra/typeorm/repositories/ProfessionalsRepository';
+import ProfessionalsSpecialtiesRepository from 'infra/typeorm/repositories/ProfessionalsSpecialtiesRepository';
+import ServiceLocationsRepository from 'infra/typeorm/repositories/ServiceLocationsRepository';
 import SpecialtiesRepository from 'infra/typeorm/repositories/SpecialtiesRepository';
 import UsersRepository from 'infra/typeorm/repositories/UsersRepository';
+import { IPaymentMethodsRepository } from 'repositories/IPaymentMethodsRepository';
 import { IPeopleRepository } from 'repositories/IPeopleRepository';
 import { IProfessionalsRepository } from 'repositories/IProfessionalsRepository';
+import { IProfessionalsSpecialtiesRepository } from 'repositories/IProfessionalsSpecialtiesRepository';
+import { IServiceLocationsRepository } from 'repositories/IServiceLocationsRepository';
 import { ISpecialtiesRepository } from 'repositories/ISpecialtiesRepository';
 import { IUsersRepository } from 'repositories/IUsersRepository';
 import { container } from 'tsyringe';
@@ -26,4 +32,19 @@ container.registerSingleton<ISpecialtiesRepository>(
 container.registerSingleton<IProfessionalsRepository>(
   'ProfessionalsRepository',
   ProfessionalsRepository,
+);
+
+container.registerSingleton<IPaymentMethodsRepository>(
+  'PaymentMethodsRepository',
+  PaymentMethodsRepository,
+);
+
+container.registerSingleton<IServiceLocationsRepository>(
+  'ServiceLocationsRepository',
+  ServiceLocationsRepository,
+);
+
+container.registerSingleton<IProfessionalsSpecialtiesRepository>(
+  'ProfessionalsSpecialtiesRepository',
+  ProfessionalsSpecialtiesRepository,
 );
